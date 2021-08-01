160d7e4129c04e3eec793201f7c177b09679d2ec
acmendoza11 <acemendoza1113@gmail.com>
2021-08-01 21:53:33 +0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import SignUp from './pages/SignUp';
 import LogIn from './pages/LogIn';
 import Logs from './pages/Logs';
+import QRCodeDisplay from './pages/QRCodeDisplay';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Header/>
           
           <Switch>
+            <Route exact path="/tracevax/qrcodedisplay" component={QRCodeDisplay} />
             <Route exact path="/tracevax/signup" component={SignUp} />
             <Route exact path="/tracevax/logIn" component={LogIn} />
             <Route exact path="/tracevax/logs" component={Logs} />
