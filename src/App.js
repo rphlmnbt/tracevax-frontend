9b45e42354e029d78139db7d70ec15b10ad7c199
acmendoza11 <acemendoza1113@gmail.com>
2021-08-01 21:52:33 +0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-d
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import 'bootstrap/dist/css/bootstrap.css'
+import SignUp from './pages/SignUp';
+import LogIn from './pages/LogIn';
+import Logs from './pages/Logs';
 import QRCodeDisplay from './pages/QRCodeDisplay';
 
 function App() {
@@ -14,7 +17,10 @@ function App() {
           
           <Switch>
             <Route exact path="/tracevax/qrcodedisplay" component={QRCodeDisplay} />
-            <Redirect from="" to="/tracevax/qrcodedisplay"/>
+            <Route exact path="/tracevax/signup" component={SignUp} />
+            <Route exact path="/tracevax/logIn" component={LogIn} />
+            <Route exact path="/tracevax/logs" component={Logs} />
+            <Redirect from="" to="/tracevax/logs"/>
           </Switch>
           <Footer />
       </Router>
