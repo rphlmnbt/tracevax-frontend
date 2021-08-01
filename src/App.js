09748aa23556d4155a572bf8811a8200fc896b28
acmendoza11 <acemendoza1113@gmail.com>
2021-08-01 21:30:57 +0800

    added sign up import

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer"
 import 'bootstrap/dist/css/bootstrap.css'
 import LogIn from './pages/LogIn';
 import Logs from './pages/Logs';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
